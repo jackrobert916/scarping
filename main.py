01ac9d642005038ef92a451d1a0e37e12d20a102
jackrobert916 <110872036+jackrobert916@users.noreply.github.com>
2022-08-22 19:55:57 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+
+from scrap import Scrape
+
+print("# Please input the URL what you want to get repository\n")
+url = str(input())
+
+# url = "https://github.com/search?q=lightgbm&type=Repositories&ref=advsearch&l=&l="
+
+print("\n# URL: ", url)
+
+print("\n# Please wait downloading files, you can check url and processes of this\n")
+cclauss = Scrape(url)
+
+if url.split("/")[len(url.split("/"))-1].split("?")[0] != "search":
+    cclauss.downloadUser()
+else:
+    print("\n # Downloading Repositories \n")
+    cclauss.downloadRepoFiles()
+
+    print("\n # downloading Commits \n")
+    cclauss.downloadCommitFiles()
+
+    print("\n # downloading Issues \n")
+    cclauss.downloadIssueFiles()
+
+    print("\n # downloading Wikis \n")
+    cclauss.downloadWikiFiles()
---
+++ scrap.py
@@ -0,0 +1,292 @@
+from zipfile import ZipFile
+
+# import Scrapping modules
+from bs4 import BeautifulSoup as bs
+from requests import get
+
+import os
+
+# https://github.com/HSE-LAMBDA (organization)
+# https://github.com/cclauss
+# https://github.com/search?q=lightgbm&type=Repositories&ref=advsearch&l=&l=
+# https://github.com/search/advanced?q=VWRegressor&type=Repositories
+# https://github.com/search?q=xgboost+time+series
+
+
+class Scrape:
+    r"""This Class is very useful to scrape website
+        You even know some tag name or class name or id name.
+        This would create the Excel file in easy weay
+        You only use on function @downloadFiles()
+        :downloadFiles() is the function what get url path and download zipfile then unzip it.
+        """
+
+    def __init__(self, _url: str):
+        r"""this function is constructor to get Institution Data by scraping from website
+        :url: website's path name, parameter type: @String
+        :_param_num: the number of parameter type: @Number
+        """
+        os.makedirs(os.getcwd() + "\\downloads\\issues", exist_ok=True)
+        os.makedirs(os.getcwd() + "\\downloads\\wikito", exist_ok=True)
+        os.makedirs(os.getcwd() + "\\downloads\\repositories", exist_ok=True)
+        os.makedirs(os.getcwd() + "\\downloads\\commits", exist_ok=True)
+
+        self.url = _url.split("?")[0]
+        try:
+            self.query = _url.split("?")[1]
+        except:
+            pass
+
+
+        sub_url = "https://github.com/orgs" + self.url.split("/")[len(_url.split("/"))-1] +"/repositories"
+        res = get(sub_url)
+        if res.status_code > 300 :
+            self.url = self.url
+        else:
+            self.url = sub_url
+
+        self.downloads = []
+        self.files = []
+
+    def downloadUser(self):
+        num = 1
+        i = 0
+        while True:
+            i = i + 1
+            repo_param = {
+                "page": str(i),
+                "tab": "repositories"
+            }
+            collection = ".wb-break-all a"
+            res = get(self.url, repo_param)
+            if res.status_code > 300:
+                break
+            soup = bs(res.content, 'html.parser')
+            down = soup.select(collection)
+            for element in down:
+                URL = "https://github.com" + element['href']
+                response = get(URL, {})
+                soup = bs(response.content, 'html.parser')
+                file_url = soup.select(
+                    "div.dropdown-menu.dropdown-menu-sw.p-0 a.no-underline")
+                for j in range(1, len(file_url), 2):
+                    url = file_url[j]["href"]
+                    print("https://github.com" + url)
+                    r = get("https://github.com" + url)
+
+                    # creating file
+                    file_name = "downloads\\repositories\\" + url.split("/")[len(url.split("/"))-6] + "_" + url.split(
+                        "/")[len(url.split("/"))-5] + "_" + url.split("/")[len(url.split("/")) - 1].split(".")[0] + "_NN" + str(num) + ".zip"
+                    num = num + 1
+
+                    # download file
+                    open(file_name, 'wb').write(r.content)
+
+                    # unzipping file
+                    try:
+                        with ZipFile(file_name, 'r') as zipObj:
+                            # Extract all the contents of zip file in different directory
+                            zipObj.extractall(file_name.split(".")[0])
+                        print("-------------" + file_name +
+                              " unziped--------------------")
+                    except:
+                        pass
+
+    def downloadRepoFiles(self):
+        i = 0
+        num = 1
+        # https://github.com/search/advanced?q=VWRegressor&type=Repositories
+        queries = self.query.split("&")
+        for ele in queries:
+            if "q=" in ele:
+                query = ele.replace("q=", "")
+        while True:
+            i = i + 1
+            repo_param = {
+                "p": str(i),
+                "q": query,
+                "type": "Repositories"
+            }
+            res = get(self.url, repo_param)
+            if res.status_code > 300:
+                break
+            soup = bs(res.content, 'html.parser')
+            down = soup.select("a.v-align-middle")
+            for element in down:
+                URL = "https://github.com" + element['href']
+                response = get(URL, {})
+                soup = bs(response.content, 'html.parser')
+                file_url = soup.select(
+                    "div.dropdown-menu.dropdown-menu-sw.p-0 a.no-underline")
+                for j in range(1, len(file_url), 2):
+                    url = file_url[j]["href"]
+                    print("https://github.com" + url)
+                    r = get("https://github.com" + url)
+                    # creating file
+                    file_name = "downloads\\repositories\\" + url.split("/")[len(url.split("/"))-6] + "_" + url.split(
+                        "/")[len(url.split("/"))-5] + "_" + url.split("/")[len(url.split("/")) - 1].split(".")[0] + "_NN" + str(num) + ".zip"
+                    num = num + 1
+
+                    # download file
+                    open(file_name, 'wb').write(r.content)
+
+                    # unzipping file
+                    try:
+                        with ZipFile(file_name, 'r') as zipObj:
+                            # Extract all the contents of zip file in different directory
+                            zipObj.extractall(
+                                file_name.split(".")[0])
+                        print("------------- " + file_name +
+                              " unziped--------------------")
+                    except Exception as err:
+                        print(err)
+
+    def downloadWikiFiles(self):
+        num = 1
+        i = 0
+        # https://github.com/search/advanced?q=VWRegressor&type=Repositories
+        queries = self.query.split("&")
+        for ele in queries:
+            if "q=" in ele:
+                query = ele.replace("q=", "")
+        while True:
+            i = i + 1
+            repo_param = {
+                "p": str(i),
+                "q": query,
+                "type": "wikis"
+            }
+            res = get(self.url, repo_param)
+            print(res.status_code)
+            if res.status_code > 300:
+                break
+            soup = bs(res.content, 'html.parser')
+            down = soup.select("a.Link--muted.text-bold")
+            for element in down:
+                URL = "https://github.com" + element['href']
+                response = get(URL, {})
+                soup = bs(response.content, 'html.parser')
+                file_url = soup.select(
+                    "div.dropdown-menu.dropdown-menu-sw.p-0 a.no-underline")
+                for j in range(1, len(file_url), 2):
+                    url = file_url[j]["href"]
+                    print("https://github.com" + url)
+                    r = get("https://github.com" + url)
+                    # creating file
+                    file_name = "downloads\\wikito\\" + url.split("/")[len(url.split("/"))-6] + "_" + url.split(
+                        "/")[len(url.split("/"))-5] + "_" + url.split("/")[len(url.split("/")) - 1].split(".")[0] + "_NN" + str(num) + ".zip"
+                    num = num + 1
+
+                    # download file
+                    open(file_name, 'wb').write(r.content)
+
+                    # unzipping file
+                    try:
+                        with ZipFile(file_name, 'r') as zipObj:
+                            # Extract all the contents of zip file in different directory
+                            zipObj.extractall(file_name.split(".")[0])
+                        print("------------- " + file_name +
+                              " unziped--------------------")
+                    except:
+                        continue
+
+    def downloadIssueFiles(self):
+        i = 1
+        num = 1
+        # https://github.com/search/advanced?q=VWRegressor&type=Repositories
+        queries = self.query.split("&")
+        for ele in queries:
+            if "q=" in ele:
+                query = ele.replace("q=", "")
+        while True:
+            i = i + 1
+            repo_param = {
+                "p": str(i),
+                "q": query,
+                "type": "issues"
+            }
+            res = get(self.url, repo_param)
+            print(res.status_code)
+            if res.status_code > 300:
+                break
+            soup = bs(res.content, 'html.parser')
+            down = soup.select("a.Link--muted.text-bold")
+            for j in range(0, len(down), 2):
+                URL = "https://github.com" + \
+                    down[j]['href'].replace("/issues", "")
+                response = get(URL, {})
+                soup = bs(response.content, 'html.parser')
+                file_url = soup.select(
+                    "div.dropdown-menu.dropdown-menu-sw.p-0 a.no-underline")
+                for k in range(1, len(file_url), 2):
+                    url = file_url[k]["href"]
+                    print("https://github.com" + url)
+                    r = get("https://github.com" + url)
+                    # creating file
+                    file_name = "downloads\\issues\\" + url.split("/")[len(url.split("/"))-6] + "_" + url.split(
+                        "/")[len(url.split("/"))-5] + "_" + url.split("/")[len(url.split("/")) - 1].split(".")[0] + "_NN" + str(num) + ".zip"
+                    num = num + 1
+
+                    # download file
+                    open(file_name, 'wb').write(r.content)
+
+                    # unzipping file
+                    try:
+                        with ZipFile(file_name, 'r') as zipObj:
+                            # Extract all the contents of zip file in different directory
+                            zipObj.extractall(file_name.split(".")[0])
+                        print("------------- " + file_name +
+                              " unziped--------------------")
+                    except:
+                        continue
+
+    def downloadCommitFiles(self):
+        i = 0
+        num = 1
+        # https://github.com/search/advanced?q=VWRegressor&type=Repositories
+        queries = self.query.split("&")
+        for ele in queries:
+            if "q=" in ele:
+                query = ele.replace("q=", "")
+        while True:
+            i = i + 1
+            repo_param = {
+                "p": str(i),
+                "q": query,
+                "type": "commits"
+            }
+            print(self.url, repo_param["p"],
+                  repo_param["q"], repo_param["type"])
+            res = get(self.url, repo_param)
+            print(res.status_code)
+            if res.status_code > 300:
+                break
+            soup = bs(res.content, 'html.parser')
+            down = soup.select("a.Link--secondary.text-bold")
+            for element in down:
+                URL = "https://github.com" + element['href']
+                response = get(URL, {})
+                soup = bs(response.content, 'html.parser')
+                file_url = soup.select(
+                    "div.dropdown-menu.dropdown-menu-sw.p-0 a.no-underline")
+                for j in range(1, len(file_url), 2):
+                    url = file_url[j]["href"]
+                    print("https://github.com" + url)
+                    r = get("https://github.com" + url)
+                    # creating file
+                    file_name = "downloads\\commits\\" + url.split("/")[len(url.split("/"))-6] + "_" + url.split(
+                        "/")[len(url.split("/"))-5] + "_" + url.split("/")[len(url.split("/")) - 1].split(".")[0] + "_NN" + str(num) + ".zip"
+                    num = num + 1
+
+                    # download file
+                    open(file_name, 'wb').write(r.content)
+
+                    # unzipping file
+                    try:
+                        with ZipFile(file_name, 'r') as zipObj:
+                            # Extract all the contents of zip file in different directory
+                            zipObj.extractall(file_name.split(".")[0])
+                        print("------------- " + file_name +
+                              " unziped--------------------")
+                    except:
+                        continue
